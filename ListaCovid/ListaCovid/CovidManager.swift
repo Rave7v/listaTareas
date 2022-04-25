b014de813fa4ffe39a422a2b13e24a1891110a8d
alexis roman <elsanguinario3257@gmail.com>
2022-04-25 09:59:37 -0500

    fin de la clase 1

---
+++ ListaCovid/ListaCovid/CovidManager.swift
@@ -0,0 +1,8 @@
+//
+//  CovidManager.swift
+//  ListaCovid
+//
+//  Created by Mac12 on 25/04/22.
+//
+
+import Foundation
---
+++ ListaCovid/ListaCovid/CovidModelo.swift
@@ -0,0 +1,8 @@
+//
+//  CovidModelo.swift
+//  ListaCovid
+//
+//  Created by Mac12 on 25/04/22.
+//
+
+import Foundation
